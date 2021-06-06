74b226244f25a06cfc62108e90f38ed0311ed8e1
Carson Noble <carsonnoble@cattlekrush.com>
2021-06-06 09:43:27 -0500

    Adjusting real-time

--- js/module/settings.m.js
+++ js/module/settings.m.js
@@ -1,7 +1,7 @@
 export const settings = {
   game_count: 162,
   inning_count: 9,
-  real_time: false,
+  real_time: true,
   auto_new_day: true,
   base_runner_extra: false,
   required_outs: (() => document.querySelector("#required_outs").value)(),
